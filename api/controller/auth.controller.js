1f3af3cb21b6680b9904ea0189da2d35fcbb2dc9
30Xp107 <jhanbrent0630@gmail.com>
2023-10-11 06:17:15 +0000

    create middleware and function for possible errors

--- api/controller/auth.controller.js
+++ api/controller/auth.controller.js
@@ -1,24 +1,16 @@
 import User from '../models/user.model.js'
 import bcrypt from 'bcryptjs'
 
-const signup = async(req, res) => {
+const signup = async(req, res, next) => {
 
     const { username, email, password } = req.body
     const hashedPassword = bcrypt.hashSync(password, 10)
     const newUser = new User({ username, email, password: hashedPassword })
-    // if(username === req.body.username) {
-    //     console.log('Duplicate Username')
-    //     return res.status(500).json({message: 'Duplicate Username'})
-    // }
-    // if(email === req.body.email) {
-    //     console.log('Duplicate Email')
-    //     return res.status(500).json({message: 'Duplicate Email'})
-    // }
     try {
         await newUser.save()
         res.status(201).json("User created successfully")
     } catch (error) {
-        res.status(500).json(error.message)
+        next(error)
     }
 
 }
